b7db9a4bda4176f68e403ce6ecf2872c0d6c66e4
Shreyash9306 <118950220+Shreyash9306@users.noreply.github.com>
2023-07-07 12:08:46 +0530

    evaluator with both minimax when 2000 or less legal moves and neural networks

---
+++ main_evaluator.py
@@ -0,0 +1,131 @@
+import chess
+import numpy as np
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+board = chess.Board()
+
+piece_values = {
+    chess.PAWN: 100,
+    chess.KNIGHT: 320,
+    chess.BISHOP: 330,
+    chess.ROOK: 500,
+    chess.QUEEN: 900,
+    chess.KING: 20000
+}
+
+model = Sequential([
+    Dense(128, activation='relu', input_shape=(64,)),
+    Dense(64, activation='relu'),
+    Dense(1)
+])
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+def board_to_features(board):
+    features = []
+    for piece_type, value in piece_values.items():
+        white_pieces = board.pieces(piece_type, chess.WHITE)
+        black_pieces = board.pieces(piece_type, chess.BLACK)
+        features.append(len(white_pieces) - len(black_pieces))
+    return np.array(features)
+
+def evaluate_position_nn(board):
+    features = board_to_features(board)
+    score = model.predict(np.array([features]))[0][0]
+    return score
+
+def minimax_alpha_beta(board, depth, alpha, beta, maximizing_player):
+    if depth == 0 or board.is_game_over():
+        return evaluate_position_nn(board)
+
+    if maximizing_player:
+        max_eval = float('-inf')
+        for move in board.legal_moves:
+            board.push(move)
+            eval = minimax_alpha_beta(board, depth - 1, alpha, beta, False)
+            board.pop()
+            max_eval = max(max_eval, eval)
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+        return max_eval
+    else:
+        min_eval = float('inf')
+        for move in board.legal_moves:
+            board.push(move)
+            eval = minimax_alpha_beta(board, depth - 1, alpha, beta, True)
+            board.pop()
+            min_eval = min(min_eval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return min_eval
+
+def evaluate_position_minimax(board):
+    depth = 4
+    return minimax_alpha_beta(board, depth, float('-inf'), float('inf'), True)
+
+def evaluate_position(board):
+    if board.legal_moves.count() <= 2000:
+        return evaluate_position_minimax(board)
+    else:
+        return evaluate_position_nn(board)
+
+def find_best_move(board):
+    if board.legal_moves.count() <= 2000:
+        best_eval = float('-inf')
+        best_move = None
+
+        for move in board.legal_moves:
+            board.push(move)
+            eval = minimax_alpha_beta(board, 4, float('-inf'), float('inf'), False)
+            board.pop()
+            if eval > best_eval:
+                best_eval = eval
+                best_move = move
+
+        return best_move
+    else:
+        best_eval = float('-inf')
+        best_move = None
+
+        for move in board.legal_moves:
+            board.push(move)
+            eval = evaluate_position_nn(board)
+            board.pop()
+            if eval > best_eval:
+                best_eval = eval
+                best_move = move
+
+        return best_move
+
+def train_neural_network():
+    # Generate training data
+    X_train = []
+    y_train = []
+    for _ in range(10000):
+        # TODO: Implement your own logic to enerate a random board state
+
+        # Evaluate the position using the current board state
+        score = evaluate_position(board)
+
+        # Append the features and evaluation score to the training data
+        X_train.append(board_to_features(board))
+        y_train.append(score)
+
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
+
+    model.fit(X_train, y_train, epochs=10)
+
+depth = 4
+
+train_neural_network()
+
+best_move = find_best_move(board)
+board.push(best_move)
+
+print("Best Move:", best_move)
+print("Position Evaluation:", evaluate_position(board))
+print(board)
